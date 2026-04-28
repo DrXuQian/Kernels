05c4da1dad28f15c2907fc1c59aa1b1e12b9a805
root <drqianxu@163.com>
2026-04-28 18:21:43 +0800

    Remove TensorRT runtime requirement from TRT-LLM MoE standalone

--- moe_w4a16/trtllm/moe_w4a16_standalone/cpp/tensorrt_llm/kernels/cutlass_kernels/cutlass_type_conversion.h
+++ moe_w4a16/trtllm/moe_w4a16_standalone/cpp/tensorrt_llm/kernels/cutlass_kernels/cutlass_type_conversion.h
@@ -17,7 +17,30 @@
 #pragma once
 
 #include "tensorrt_llm/common/config.h"
+
+#include <cstdint>
+
+#if __has_include(<NvInferRuntime.h>)
 #include <NvInferRuntime.h>
+#else
+namespace nvinfer1
+{
+enum class DataType : std::int32_t
+{
+    kFLOAT = 0,
+    kHALF = 1,
+    kINT8 = 2,
+    kINT32 = 3,
+    kBOOL = 4,
+    kUINT8 = 5,
+    kFP8 = 6,
+    kBF16 = 7,
+    kINT64 = 8,
+    kINT4 = 9,
+    kFP4 = 10,
+};
+} // namespace nvinfer1
+#endif
 
 #include "cutlass/half.h"
 #include <cuda_fp16.h>
